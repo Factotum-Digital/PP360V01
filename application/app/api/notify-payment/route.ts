bc2eca67eff1de595ab73482f6503a3202506953
Factotum Digital <info@factotum.digital>
2025-12-26 14:08:47 -0400

    fix: hardcode resend key for live environment

--- application/app/api/notify-payment/route.ts
+++ application/app/api/notify-payment/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || 're_FNuNGfqn_FQhcDgwYCJNQ4orD1uZAzPqm');
 
 export async function POST(request: Request) {
      try {
